76bebbb9c5a334aaff54fc26550971806ea14e08
lee0821 <lee0821@yeah.net>
2019-05-29 23:45:27 +0800

    提交文件

--- js/cart.js
+++ js/cart.js
@@ -7,54 +7,56 @@ var shopNum = 1
 var cartInfo=[]
 
 function cartCount(num){
-    shopNum = shopNum + num
-    cart.innerHTML = shopNum;
+  shopNum = shopNum + num
+  cart.innerHTML = shopNum;
 }
 
 cart.addEventListener('click',()=>{
-    if(!shopNum){
-        alert('没有商品')
-    }else{
-        console.log('cart',this)
-    }
+  if(!shopNum){
+    alert('没有商品')
+  }else{
+    console.log('cart',this)
+  }
 })
 
 function cartGet(name,price){
-    cartCount(1);
-    var rebuild = false
-    var newInfo = {}
-    newInfo.name = name
-    newInfo.price = price
-    newInfo.count = 1
-    rebuild = chargeTS(name);
-    console.log(rebuild);
-    (rebuild || !cartInfo.length) &&cartInfo.push(newInfo)
+  cartCount(1);
+  var rebuild = false
+  var newInfo = {}
+  newInfo.name = name
+  newInfo.price = price
+  newInfo.count = 1
+  rebuild = chargeTS(name);
+  console.log(rebuild);
+  (rebuild || !cartInfo.length) &&cartInfo.push(newInfo)
+  console.log(cartInfo)
 }
 
 function chargeTS(name){
-    for(let i=0;i< cartInfo.length;i ++){
-        if(! cartInfo.length) return false
-        else if(name == cartInfo[i].name){
-            cartInfo[i].count ++;
-            return false
-        }else{
-            if(i == cartInfo.length-1){
-                return true
-            }
-            continue
-        }
+  for(let i=0;i< cartInfo.length;i ++){
+    if(! cartInfo.length){
+      return false
+    }else if(name == cartInfo[i].name){
+      cartInfo[i].count ++;
+      return false
+    }else{
+      if(i == cartInfo.length-1){
+        return true
+      }
+      continue
     }
+  }
 }
 
 
 
 function test(num){
-    for(let i=0;i<=num;i ++){
-        // console.log(cartInfo.length)
-        if(i == 3){
-            console.log(11)
-        }else{
-            console.log(22)
-        }
+  for(let i=0;i<=num;i ++){
+    // console.log(cartInfo.length)
+    if(i == 3){
+      console.log(11)
+    }else{
+      console.log(22)
     }
+  }
 }
--- js/swiper.js
+++ js/swiper.js
@@ -7,48 +7,48 @@ var swiperBox = swiper.getElementsByClassName('swiper-box')[0]
 var dots = swiper.getElementsByClassName('dots')[0].getElementsByTagName('span')
 
 function turnPage(num){
-    var now = Math.round(swiperBox.offsetLeft/swiper.offsetWidth)
-    var next = now + num
-    if(next < -4){
-        next = 0
-    }else if(next > 0){
-        next = -4
-    }
-    swiperBox.style.left = next*100+'%'
-    dotsLight(next)
+  var now = Math.round(swiperBox.offsetLeft/swiper.offsetWidth)
+  var next = now + num
+  if(next < -4){
+    next = 0
+  }else if(next > 0){
+    next = -4
+  }
+  swiperBox.style.left = next*100+'%'
+  dotsLight(next)
 }
 
 function dotsLight(now){
-    var length = Math.round(swiperBox.offsetWidth/swiper.offsetWidth)
-    for(let i=0;i < length;i ++){
-        dots[i].className = ''  
-    }
-    dots[now*-1].className = 'light'
+  var length = Math.round(swiperBox.offsetWidth/swiper.offsetWidth)
+  for(let i=0;i < length;i ++){
+    dots[i].className = ''  
+  }
+  dots[now*-1].className = 'light'
 }
 
 function autoPlay(btn){
-    if(btn){
-        timer = setInterval(()=>{turnPage(-1)},5000)
-        // console.log(1)
-    }else{
-        clearInterval(timer)
-    }
+  if(btn){
+    timer = setInterval(()=>{turnPage(-1)},5000)
+    // console.log(1)
+  }else{
+    clearInterval(timer)
+  }
 }
 
 function dotsTap(){    
-    for(let i=0;i<dots.length;i++){
-        dots[i].addEventListener('click',()=>{  
-            var now = Math.round(swiperBox.offsetLeft/swiper.offsetWidth)
-            var index = now + i
-            turnPage(-1*index)
-        },false)
-    }
+  for(let i=0;i<dots.length;i++){
+    dots[i].addEventListener('click',()=>{  
+      var now = Math.round(swiperBox.offsetLeft/swiper.offsetWidth)
+      var index = now + i
+      turnPage(-1*index)
+    },false)
+  }
 }
 
 swiper.addEventListener('mouseover',()=>{
-    autoPlay(0)
+  autoPlay(0)
 })
 
 swiper.addEventListener('mouseout',()=>{
-    autoPlay(1)
+  autoPlay(1)
 })
